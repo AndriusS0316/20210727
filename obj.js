5a6fc38805f80b2f72d5a741aa150475f8d8d660
Andrius <andriuss0316@gmail.com>
2021-07-28 09:12:16 +0300

    20210728

--- obj.js
+++ obj.js
@@ -1,44 +1,44 @@
-class Zmogus {
-    constructor(vardas) {
-        this.vardas = vardas;
-    }
+// class Zmogus {
+//     constructor(vardas) {
+//         this.vardas = vardas;
+//     }
 
-    labas() {
-        console.log("Labas " + this.vardas);
-    }
+//     labas() {
+//         console.log("Labas " + this.vardas);
+//     }
+// }
+
+// class Studentas extends Zmogus {
+
+//     constructor() {
+//         super(...arguments);
+//         this.pazymiai = [];
+//     }
+// }
+
+function Zmogus(vardas) {
+    this.vardas = vardas;
 }
 
-class Studentas extends Zmogus {
-
-    constructor() {
-        super(...arguments);
-        this.pazymiai = [];
-    }
+Zmogus.prototype.labas = function () {
+    console.log("Labas " + this.vardas);
+}
+Zmogus.prototype.blogasLabas = () => {
+    console.log(this);
+    console.log(global);
+    console.log("Labas " + this.vardas);
 }
 
-// function Zmogus(vardas) {
-// this.vardas = vardas;
-// }
-
-// Zmogus.prototype.labas = function() {
-// console.log("Labas " + this.vardas);
-// }
-// Zmogus.prototype.blogasLabas = () => {
-// console.log(this);
-// console.log(global);
-// console.log("Labas " + this.vardas);
-// }
-
-// function Studentas() {
-// Zmogus.apply(this, arguments);
-// this.pazymiai = [];
-// }
+function Studentas() {
+    Zmogus.apply(this, arguments);
+    this.pazymiai = [];
+}
 
 let s = new Studentas("Petras");
 console.log(s);
 s.labas();
 
-   // let z = new Zmogus("Jonas");
+let z = new Zmogus("Jonas");
 
-   // z.labas();
-   // z.blogasLabas();
+z.labas();
+z.blogasLabas();
