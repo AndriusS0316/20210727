4eead0a77be4a0e1ab4673cde4134152c4053e85
Andrius <andriuss0316@gmail.com>
2021-07-28 10:56:26 +0300

    20210728

---
+++ ciklai.js
@@ -0,0 +1,34 @@
+
+
+
+let a = 7;
+
+while (a < 3) {
+    console.log(a++);
+    
+}
+console.log("labas");
+
+// if (a < 100) {
+//     console.log("mazas");
+//     console.log("skaicius");
+// } else {
+//     console.log("didelis");
+//     console.log("sk");
+// }
+
+
+// if (a < 0) {
+//     console.log("neigiamas");
+// } else 
+//     if (a === 0) 
+//         console.log("nulis");
+//     else 
+//         if (a < 10) 
+//             console.log("mazas");
+//         else 
+//             if (a < 100) {
+//                 console.log("vidutinis");
+//             } else {
+//                 console.log("didelis");
+//             }
---
+++ switch.js
@@ -0,0 +1,48 @@
+
+let a = 6;
+
+switch (a) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("darbo diena");
+        break;
+    case 6:
+    case 7:
+        console.log("savaitgalis");
+        break;
+    default:
+        console.log("nera tokios savaites dienos");
+        break;
+}
+
+
+// switch (a) {
+//     default:
+//         console.log("nera tokios savaites dienos");
+//         break;
+//     case 1:
+//         console.log("pirmadienis");
+//         break;
+//     case 2:
+//         console.log("antradienis");
+//         break;
+//     case 3:
+//         console.log("treciadienis");
+//         break;
+//     case 4:
+//         console.log("ketvirtadienis");
+//         break;
+//     case 5:
+//         console.log("penktadienis");
+//         break;
+//     case 6:
+//         console.log("sestadienis");
+//         break;
+//     case 7:
+//         console.log("sekmadienis");
+//         break;
+// }
+console.log("pabaiga");
