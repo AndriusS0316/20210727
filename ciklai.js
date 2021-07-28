b13b487b6c7f750f64aa01d373db6e60c8f1d3cf
Andrius <andriuss0316@gmail.com>
2021-07-28 11:24:41 +0300

    20210728

--- ciklai.js
+++ ciklai.js
@@ -1,13 +1,16 @@
 
 
 
-let a = 7;
+// let a = 7;
 
-while (a < 3) {
-    console.log(a++);
+
+
+
+// while (a < 3) {
+//     console.log(a++);
     
-}
-console.log("labas");
+// }
+// console.log("labas");
 
 // if (a < 100) {
 //     console.log("mazas");
@@ -20,15 +23,18 @@ console.log("labas");
 
 // if (a < 0) {
 //     console.log("neigiamas");
-// } else 
-//     if (a === 0) 
-//         console.log("nulis");
-//     else 
-//         if (a < 10) 
-//             console.log("mazas");
-//         else 
-//             if (a < 100) {
-//                 console.log("vidutinis");
-//             } else {
-//                 console.log("didelis");
-//             }
+// } else if (a === 0) 
+//     console.log("nulis");
+// else if (a < 10) 
+//     console.log("mazas");
+// else if (a < 100) {
+//     console.log("vidutinis");
+// } else {
+//     console.log("didelis");
+// }
+
+for (let i = 1; i < 3; i++) {
+    let j = 3;
+    console.log(i + j);
+}
+console.log("pabaiga", i);
---
+++ funkcijos.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function testas() {
+    console.log("testas");
+}
+
+testas();
+
+testas = function labas() {
+    console.log("labas");
+};
+
+testas();
+
+labas();
