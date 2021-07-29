2602218f843a4e1f1e3ceac027de6ac8d51453d6
Andrius <andriuss0316@gmail.com>
2021-07-29 09:38:34 +0300

    20210729

---
+++ obij.js
@@ -0,0 +1,50 @@
+let puodukas = {
+    spalva: "baltas",
+    turis: 250,
+    kiekis: 0
+};
+
+let kitasPuodukas = {
+    spalva: "rudas",
+    turis: 300,
+    kiekis: 0
+};
+
+
+
+
+// let o = {};
+
+// console.log(o.labas);
+
+// o.labas = "hello";
+
+// console.log(o.labas);
+
+// o.labas = "hi";
+
+// console.log(o.labas);
+
+// console.log(o);
+
+// o.savybe = "reiksme";
+
+// console.log(o);
+
+// let pav = "labas";
+
+// console.log(o[pav]);
+
+// console.log(o["savybe"]);
+
+// o.kitaSavybe = 654;
+
+// console.log(o);
+
+// delete o.savybe;
+
+// console.log(o);
+
+// delete o["kitaSavybe"];
+
+// console.log(o);
