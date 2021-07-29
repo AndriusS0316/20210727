496d51db31c2ad432ebec2efd7a22068270cebe8
Andrius <andriuss0316@gmail.com>
2021-07-29 09:45:01 +0300

    20210729

--- obij.js
+++ obij.js
@@ -1,15 +1,40 @@
-let puodukas = {
-    spalva: "baltas",
-    turis: 250,
-    kiekis: 0
-};
+function Puodukas(spalva, turis) {
+    this.spalva = spalva;
+    this. turis = turis;
+    this.kiekis = 0;
+}
 
-let kitasPuodukas = {
-    spalva: "rudas",
-    turis: 300,
-    kiekis: 0
-};
+let p1 = new Puodukas ("baltas", 250);
+let p2 = new Puodukas ("rudas", 300);
 
+console.log(p1, p2);
+
+// function suma (a, b) {
+//     return a + b;
+// }
+
+// let s = new suma();
+
+// console.log(s);
+
+
+
+
+// console.log("----------------");
+
+// let puodukas = {
+//     spalva: "baltas",
+//     turis: 250,
+//     kiekis: 0
+// };
+
+// let kitasPuodukas = {
+//     spalva: "rudas",
+//     turis: 300,
+//     kiekis: 0
+// };
+
+// console.log(puodukas, kitasPuodukas);
 
 
 
