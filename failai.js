46a672fff9e3e2ba6910aca0efc1699dc78aa04d
Andrius <andriuss0316@gmail.com>
2021-08-04 10:46:38 +0300

    20210804

--- failai.js
+++ failai.js
@@ -5,15 +5,15 @@ function readFile(name, options) {
             if (err) {
                 reject(err);
                 return;
-            }
+            };
             resolve(data);
         });
     });
-}
+};
 let text = "";
 let p1 = readFile("a.txt", {
     encoding: "utf8"
-})
+});
 let p2 = p1.then(data => {
     console.log("perskaiciau is failo");
     console.log(data);
@@ -21,12 +21,12 @@ let p2 = p1.then(data => {
     return readFile("b.txt", {
         encoding: "utf8"
     });
-})
+});
 let p3 = p2.then(data => {
     console.log("perskaiciau is kito failo");
     console.log(data);
     text += data;
-})
+});
 let p4 = p3.catch(err => {
     console.log("klaida skaitant is failo");
     console.log(err);
@@ -43,3 +43,5 @@ console.log("p2", p2);
 console.log("p3", p3);
 console.log("p4", p4);
 console.log("main'as baige");
+
+
